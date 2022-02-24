d4f36136764973e7cfd9fce71596c8096b2450df
Jin Hu, Baek <m@ake.kr>
2022-02-25 03:12:29 +0900

    formatting update

--- database/djmax-respect-v/config.ts
+++ database/djmax-respect-v/config.ts
@@ -3,8 +3,7 @@ import { Configuration, ItemData } from '../../common/interface'
 export const gameConfig = {
   modes: ['4K', '5K', '6K', '8K'] as const,
   difficulties: ['NORMAL', 'HARD', 'MAXIMUM', 'SC'] as const,
-  metadataKeys: ['category', 'bpm', 'totalNotes'] as const,
-  base: '2022-01-01'
+  metadataKeys: ['category', 'bpm', 'totalNotes'] as const
 }
 
 export type modes = typeof gameConfig.modes[number]
@@ -30,5 +29,10 @@ export interface DMRVData extends Omit<ItemData, 'metadata'> {
 }
 
 export interface DMRVRankTable {
-  [key: string]: DMRVData
+  date: string,
+  data: {
+    insert?: Array<DMRVData>,
+    update?: { [key: string]: DMRVData },
+    delete?: Array<string>
+  }
 }
--- database/djmax-respect-v/data.ts
+++ database/djmax-respect-v/data.ts
@@ -1,4 +1,10 @@
 import { DMRVRankTable } from './config'
 
 export const data: DMRVRankTable = {
+  date: new Date().toString(),
+  data: {
+    // create: [],
+    // update: {},
+    // delete: []
+  }
 }
